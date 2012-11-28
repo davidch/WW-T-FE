c3ecb9cda5408406492b34f51bc728726577d67e
David Choi <TouroMac@Davids-iMac.local>
2012-11-28 11:39:46 -0500

    first commit

---
+++ about-inside.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The Touro College and University System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">    
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/touro-custom.css" rel="stylesheet">
+    <link href="css/touro-media.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body class="body_about inside">
+
+	  <?php include('includes/top-navigation.php'); ?>
+	  
+	  <div class="container">
+	  	<?php include('includes/search-inside-pages.php'); ?>
+	  
+	  	<?php include('includes/main-navigation.php'); ?>
+	  </div>	
+	  
+	  
+	<!-- scroll area -->
+	<div class="scroll_bar">
+		<div id="makeMeScrollable">
+			<div>
+				<div class="showInfo">
+					<img src="/buildout/images/inside.jpg" />
+					<div class="scroll-overlay-text">
+						<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+					</div>	
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+			<div>	
+				<div class="showInfo">
+					<img src="/buildout/images/inside-02.jpg" />
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+			<div>
+				<div class="showInfo">
+					<img src="/buildout/images/inside-03.jpg" />
+					<div class="scroll-overlay-text">
+						<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+					</div>	
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+			<div>
+				<div class="showInfo">
+					<img src="/buildout/images/inside-04.jpg" />
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+			<div>	
+				<div class="showInfo">
+					<img src="/buildout/images/inside.jpg" />
+					<div class="scroll-overlay-text">
+						<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+					</div>	
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+			<div>
+				<div class="showInfo">
+					<img src="/buildout/images/inside-02.jpg" />
+					<div class="scroll-overlay"></div>
+				</div>
+			</div>
+		</div>
+	</div>	  
+
+    <div class="container" id="main_content">
+	    <div class="row">
+	        <div class="span3 section_navigation">
+	        	<h2>About Touro</h2>
+    			<ul>
+    				<li><a href="/buildout/about-inside.php" target="_blank">At a Glance</a></li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Research</a></li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Our Mission</a></li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Our History</a></li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Jewish Heritage</a></li>
+					<li class="active"><a href="/buildout/about-inside.php" target="_blank">Leadership</a>
+						<ul class="subnav">
+							<li><a href="#">Office of the President</a></li>
+							<li><a href="#">Central Administration</a></li>
+							<li><a href="#">Board of Trustees</a></li>
+							<li><a href="#">Legal</a></li>
+						</ul>
+					</li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Accreditations</a></li>
+					<li><a href="/buildout/about-inside.php" target="_blank">Careers at Touro</a></li>
+				</ul>
+			</div>
+			<div class="span9 main">
+				<h2 class="section_header">Leadership</h2>
+				<div class="row">
+					<div class="span6">	
+						<p class="intro">Sed ut perspiciatis unde omnis iste natus error sit accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta, Touro College sunt explicabo.</p>
+						<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Touro por rom quisquam est, qui dolorem New York ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius.</p>
+						<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+						<p>In justo diam, mollis eget convallis at, aliquam a sapien. Proin sollicitudin condimentum convallis. Nullam vehicula egestas odio, et consequat enim aliquet quis. Nunc a tortor in est vehicula convallis. Maecenas pellentesque metus eget mauris ultrices eget fermentum velit vehicula. Sed porttitor lorem vitae ligula sollicitudin aliquam. Nulla sed tincidunt lacus.</p>
+						<ul>
+							<li><a href="#">More Information</a></li>
+							<li><a href="#">Schedule a Visit</a></li>
+							<li><a href="#">Campus Activities</a></li>
+						</ul>
+						<p>Sed in lectus et nisl tempus elementum eu nec arcu. Nullam a aliquet lacus. Proin id felis nec enim consequat ultricies vitae sed ipsum. Duis sodales imperdiet pharetra. Curabitur suscipit lectus at diam facilisis ac dignissim eros dapibus. </p>
+					</div>	
+					<div class="span3 sidebar_feature">
+						<img src="/buildout/images/lamar-miller.jpg" alt="Treating History" />							
+						<div class="img_overlay">
+						</div>	
+						<div class="inner">
+							<h3>Touro People:Graduate School of Education Dean <span>LaMar Miller</span></h3>
+							<p>Dr. LaMar P. Miller, dean of the Graduate School of Education, distinguished professor and founding director of the Lander Center for Educational Research, is a nationally recognized educator who spent 30 years at New York University in a number of high-profile teaching and administrative positions before joining Touro College.</p>
+							<a href="#">Full profile &raquo;</a>
+						</div>	
+					</div>
+				</div>	
+			</div>
+		</div><!-- end row -->	      
+   </div> <!-- /container -->
+
+	<footer class="clear">
+		<?php include('includes/footer.php'); ?>
+	</footer>
+
+
+
+  </body>
+</html>
---
+++ fields-of-study.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>The Touro College and University System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">    
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/touro-custom.css" rel="stylesheet">
+    <link href="css/touro-media.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body class="body_fields inside_landing">
+
+	  <?php include('includes/top-navigation.php'); ?>
+	  
+	  <div class="container">
+	  	<?php include('includes/search-inside-pages.php'); ?>
+	  
+	  	<?php include('includes/main-navigation.php'); ?>
+	  </div>	
+	  
+	  
+<!-- scroll area -->
+	<div id="makeMeScrollable">
+		<div>
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03-02.jpg" />
+				<div class="scroll-overlay-text">
+					<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+				</div>	
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+		<div>	
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03-03.jpg" />
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+		<div>
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03.jpg" />
+				<div class="scroll-overlay-text">
+					<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+				</div>	
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+		<div>
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03-02.jpg" />
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+		<div>	
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03-03.jpg" />
+				<div class="scroll-overlay-text">
+					<p>"My orthopedic surgeon told me, if you really want to <a href="#">help people</a>, go into physical therapy."</p>
+				</div>	
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+		<div>
+			<div class="showInfo">
+				<img src="/buildout/images/mosaic/inside_landing_03.jpg" />
+				<div class="scroll-overlay"></div>
+			</div>
+		</div>
+	</div>
+  
+
+    <div class="container" id="main_content">
+	    <div class="row">
+	        <div class="span3 section_navigation">
+	        	<h2>Fields of Study</h2>
+	        	<?php include('includes/section-navigation.php'); ?>
+	        </div>
+	        <div class="span9 main">
+	        	<h2 class="section_header">Medical &amp; Health Sciences</h2>
+	        	<p class="intro">Sed ut perspiciatis unde omnis iste natus error sit accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta, Touro College sunt explicabo.</p>
+	        	<div class="breadcrumbs">
+	        		<?php include('includes/breadcrumbs.php'); ?>
+	        	</div>
+	        	<div class="row"> 
+		        	<div class="span4">
+						<h3>Osteopathic Medicine</h3>
+						<p><em>Osteopathic physicians</em> lectus et nisl tempus elementum eu nec arcu. Nullam a aliquet lacus. Proin id felis nec enim consequat ultricies vitae sed ipsum. Duis sodales imperdiet pharetra. Curabitur suscipit lectus at diam facilisis ac dignissim eros dapibus.</p>
+						<h4>Doctor of Osteopathic Medicine (DO)</h4>
+						<ul>
+							<li><a href="#">Touro College of Osteopathic Medicine</a></li>
+							<li><a href="#">Touro University California College of Osteopathic Medicine</a></li>
+						</ul>
+						<br />
+						<h3>Nursing</h3>
+						<p><em>Nurses</em> lectus et nisl tempus elementum eu nec arcu. Nullam a aliquet lacus. Proin id felis nec enim consequat ultricies vitae sed ipsum. Duis sodales imperdiet pharetra. Curabitur suscipit lectus at diam facilisis ac dignissim eros dapibus.</p>
+						<h4>Registered Nurse (RN)</h4>
+						<ul>
+							<li><a href="#">Lander College for Women</a></li>
+							<li><a href="#">School of Health Sciences</a></li>
+						</ul>	   	
+		        	</div><!-- end span3 -->
+		        	<div class="span1"></div>
+		        	<div class="span4">
+						<h3>Physical Therapy</h3>
+						<p><em>Physical therapists</em> aliquet lacus. Proin id felis nec enim consequat ultricies vitae sed ipsum. Duis sodales imperdiet pharetra. Curabitur suscipit lectus at diam facilisis ac dignissim eros dapibus lectus et nisl tempus elementum eu nec arcu.</p>
+						
+						<h4>Doctor of Physical Therapy (DPT)</h4>
+						<ul>
+							<li><a href="#">School of Health Sciences</a></li>
+							<li><a href="#">Touro University California College of Education and Health Sciences</a></li>
+							<li><a href="#">Touro University Nevada College of Health and Human Services</a></li>
+						</ul>	
+						<h4>Masters in Physical Therapy (MPT)</h4>
+						<ul>
+							<li><a href="#">School of Health Sciences</a></li>
+						</ul>	
+						<h4>Undergraduate Degrees</h4>
+						<ul>
+							<li><a href="#">Lander College for Women</a></li>
+							<li><a href="#">Lander College for Men</a></li>
+						</ul>
+						<br />
+						<h3>Pharmacy</h3>
+						<p><em>Pharmacists</em> sodales imperdiet pharetra. Curabitur suscipit lectus at diam facilisis ac dignissim eros dapibus lectus et nisl tempus elementum eu aliquet lacus. tricies vitae sed ipsum. Duis nec arcu.</p>
+						<h4>Doctor of Pharmacy (PharmD)</h4>
+						<ul>
+							<li><a href="#">Touro College of Pharmacy</a></li>
+							<li><a href="#">Touro University California College of Pharmacy</a></li>			
+						</ul>				
+		        	</div><!-- end span3 -->
+		        </div><!-- end row -->	
+	        </div><!-- end main --> 
+	   </div><!-- end row -->	      
+   </div> <!-- /container -->
+
+	<footer class="clear">
+		<?php include('includes/footer.php'); ?>
+	</footer>
+
+
+
+  </body>
+</html>
