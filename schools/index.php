c19da4746c2f75074493e50c7338ca60ddcead76
Isaac McGowan <isaac@whitewhale.net>
2013-04-26 11:47:20 -0700

    Modified schools index

--- schools/index.php
+++ schools/index.php
@@ -39,16 +39,16 @@
 		    width: 10px;
 		}
  	
-		ol ol {
-		    list-style-type: lower-alpha;
-		    margin-bottom: 20px;
+		ol {
+			/* list-style-type: lower-alpha; */
+			margin-bottom: 20px;
 		} 
 		
-			ol li ol li {
+			ol li {
 			    line-height: 32px;
 			}		
 			
-				ol ol ol {
+				ol ol {
 					display: none;
 				    list-style-type: circle;
 				    margin-bottom: 10px;
@@ -76,134 +76,125 @@
 		<br class="clear" />
 		<br class="clear" />
 		<ol>
-			<li class="in-progress"><h4 class="in-progress">Religious Homepage Classic</h4>
-				<ol>
-                    <li class="in-progress"><a target="_blank" href="/schools/lcw/index.php">LCW Classic Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides in-progress">
-							<li><a target="_blank" href="/schools/lcw/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/lcw/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/lcw/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/lcw/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/lcw/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/lcw/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/lcw/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/lcw/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/lcw/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/lcw/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/lcw/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/lcw/library.php">Library</a></li>
-						</ol>						
-					</li>
-					<li class="in-progress"><a target="_blank" href="/schools/gssw/index.php">GSSW Classic Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides">
-							<li><a target="_blank" href="/schools/gssw/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/gssw/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/gssw/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/gssw/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/gssw/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/gssw/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/gssw/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/gssw/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/gssw/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/gssw/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/gssw/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/gssw/library.php">Library</a></li>
-						</ol>						
-					</li>
-					<li class="in-progress"><a target="_blank" href="/schools/shs/index.php">SHS Classic Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides">
-							<li><a target="_blank" href="/schools/shs/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/shs/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/shs/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/shs/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/shs/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/shs/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/shs/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/shs/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/shs/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/shs/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/shs/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/shs/library.php">Library</a></li>
-						</ol>						
-					</li>
+			<li class="in-progress"><a target="_blank" href="/schools/lcw/index.php">LCW Classic Mosaic</a><a class="inside" href="#inside">+</a>
+				<ol class="insides in-progress">
+					<li><a target="_blank" href="/schools/lcw/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/lcw/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/lcw/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/lcw/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/lcw/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/lcw/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/lcw/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/lcw/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/lcw/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/lcw/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/lcw/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/lcw/library.php">Library</a></li>
+				</ol>						
+			</li>
+			<li class="in-progress"><a target="_blank" href="/schools/gssw/index.php">GSSW Classic Mosaic</a><a class="inside" href="#inside">+</a>
+				<ol class="insides">
+					<li><a target="_blank" href="/schools/gssw/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/gssw/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/gssw/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/gssw/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/gssw/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/gssw/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/gssw/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/gssw/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/gssw/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/gssw/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/gssw/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/gssw/library.php">Library</a></li>
+				</ol>						
+			</li>
+			<li class="in-progress"><a target="_blank" href="/schools/shs/index.php">SHS Classic Mosaic</a><a class="inside" href="#inside">+</a>
+				<ol class="insides">
+					<li><a target="_blank" href="/schools/shs/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/shs/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/shs/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/shs/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/shs/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/shs/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/shs/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/shs/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/shs/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/shs/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/shs/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/shs/library.php">Library</a></li>
+				</ol>						
+			</li>
 
-					<li class="in-progress"><a target="_blank" href="/schools/lcm/index.php">LCM Classic Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides">
-							<li><a target="_blank" href="/schools/lcm/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/lcm/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/lcm/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/lcm/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/lcm/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/lcm/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/lcm/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/lcm/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/lcm/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/lcm/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/lcm/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/lcm/library.php">Library</a></li>
-						</ol>						
-					</li>
-					
-					<li class="in-progress"><a target="_blank" href="/schools/gse/index.php">GSE Classic Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides">
-							<li><a target="_blank" href="/schools/gse/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/gse/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/gse/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/gse/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/gse/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/gse/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/gse/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/gse/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/gse/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/gse/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/gse/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/gse/library.php">Library</a></li>
-						</ol>						
-					</li>					
-                    <li class="ns"><a target="_blank" href="/schools/touro-law/index.php">Touro Law Mosaic</a><a class="inside" href="#inside">+</a>
-						<ol class="insides">
-							<li><a target="_blank" href="/schools/touro-law/news-story.php">News Story</a></li>
-							<li><a target="_blank" href="/schools/touro-law/profile.php">Profile</a></li>
-							<li><a target="_blank" href="/schools/touro-law/event-detail.php">Event Detail</a></li>
-							<li><a target="_blank" href="/schools/touro-law/gallery.php">Photo Gallery</a></li>
-							<li><a target="_blank" href="/schools/touro-law/gateway.php">Gateway Page</a></li>
-							<li><a target="_blank" href="/schools/touro-law/department-home.php">Department/Program Homepage</a></li>
-							<li><a target="_blank" href="/schools/touro-law/department-inside.php">Department/Program Inside Page</a></li>
-							<li><a target="_blank" href="/schools/touro-law/search-results.php">Search Results</a></li>
-							<li><a target="_blank" href="/schools/touro-law/joblisting.php">Job Posting</a></li>
-							<li><a target="_blank" href="/schools/touro-law/openhouse.php">Open House Form</a></li>
-							<li><a target="_blank" href="/schools/touro-law/form.php">Form</a></li>
-							<li><a target="_blank" href="/schools/touro-law/library.php">Library</a></li>
-						</ol>						
-					</li>					
-				</ol>	
+			<li class="in-progress"><a target="_blank" href="/schools/lcm/index.php">LCM Classic Mosaic</a><a class="inside" href="#inside">+</a>
+				<ol class="insides">
+					<li><a target="_blank" href="/schools/lcm/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/lcm/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/lcm/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/lcm/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/lcm/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/lcm/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/lcm/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/lcm/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/lcm/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/lcm/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/lcm/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/lcm/library.php">Library</a></li>
+				</ol>						
 			</li>
-			<li class="in-progress"><h4 class="in-progress">Religious Homepage Doors</h4>
-				<ol>
-					<li class="in-progress"><a target="_blank" href="/schools/lcas/index.php">LCAS Doors Mosaic</a></li>
-                    <li class="in-progress"><a target="_blank" href="/schools/touro-south/index.php">Touro South Doors Mosaic</a></li>							
-				</ol>
-			</li>
-			<li class="in-progress"><a target="_blank" href="/schools/touro-com/index.php">Touro COM</a></li>			
+			
+			<li class="in-progress"><a target="_blank" href="/schools/gse/index.php">GSE Classic Mosaic</a><a class="inside" href="#inside">+</a>
+				<ol class="insides">
+					<li><a target="_blank" href="/schools/gse/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/gse/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/gse/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/gse/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/gse/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/gse/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/gse/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/gse/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/gse/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/gse/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/gse/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/gse/library.php">Library</a></li>
+				</ol>						
+			</li>					
+			<li class="ns"><a target="_blank" href="/schools/touro-law/index.php">Touro Law Mosaic</a>
+				<a class="inside" href="#inside">+</a>
+				<ol class="insides">
+					<li><a target="_blank" href="/schools/touro-law/news-story.php">News Story</a></li>
+					<li><a target="_blank" href="/schools/touro-law/profile.php">Profile</a></li>
+					<li><a target="_blank" href="/schools/touro-law/event-detail.php">Event Detail</a></li>
+					<li><a target="_blank" href="/schools/touro-law/gallery.php">Photo Gallery</a></li>
+					<li><a target="_blank" href="/schools/touro-law/gateway.php">Gateway Page</a></li>
+					<li><a target="_blank" href="/schools/touro-law/department-home.php">Department/Program Homepage</a></li>
+					<li><a target="_blank" href="/schools/touro-law/department-inside.php">Department/Program Inside Page</a></li>
+					<li><a target="_blank" href="/schools/touro-law/search-results.php">Search Results</a></li>
+					<li><a target="_blank" href="/schools/touro-law/joblisting.php">Job Posting</a></li>
+					<li><a target="_blank" href="/schools/touro-law/openhouse.php">Open House Form</a></li>
+					<li><a target="_blank" href="/schools/touro-law/form.php">Form</a></li>
+					<li><a target="_blank" href="/schools/touro-law/library.php">Library</a></li>
+				</ol>						
+			</li>					
+
+			<li class="in-progress"><a target="_blank" href="/schools/lcas/index.php">LCAS Doors Mosaic</a></li>
+
+			<li class="in-progress"><a target="_blank" href="/schools/touro-south/index.php">Touro South Doors Mosaic</a></li>							
+			<li class="in-progress"><a target="_blank" href="/schools/touro-com/index.php">Touro COM</a></li>
 		</ol>
-	 
 	</div> <!-- /container -->
 
-	<footer class="clear">
-		<?php include('../includes/footer.php'); ?>
-	</footer>
+<footer class="clear">
+	<?php include('../includes/footer.php'); ?>
+</footer>
 
 <script type="text/javascript">
 
-	$(document).ready(function(){
-		
-		$('a.inside').click(function(e){
-			
+	$(document).ready(function() {
+		$('a.inside').click(function(e) {
 			e.preventDefault();
 			$(this).siblings('ol').slideToggle();
 			
 		});
-		
 	});
 
 </script>
